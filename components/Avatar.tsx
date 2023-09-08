badfd51667f20b919b18755b26ea2d5e31cfb6db
Yana Burdakova <bu.asti@gmail.com>
2023-09-08 12:26:28 -0400

    Hero block is done

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Avatar() {
   return (
-    <div>Avatar</div>
+    <div className='hidden xl:flex xl:max-w-none'>
+      <Image
+        src={'/avatar.png'}
+        alt='Avatar'
+        width={737}
+        height={678}
+        className='w-full h-full translate-z-0'
+      />
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,19 +10,62 @@ function Home() {
   return (
     <div className='h-full bg-primary/60'>
       <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        {/* Hero text block */}
         <div className='container flex flex-col justify-center h-full mx-auto text-center xl:pt-40 xl:text-left'>
-          <h1 className="h1">
+          {/* Title */}
+          <motion.h1 
+            variants={fadeIn('down', 0.2)} 
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="h1"
+          >
             Transforming Ideas <br /> Intro {' '}
             <span className="text-accent">Digital Reality</span>
-          </h1>
-          <p className='max-w-sm mx-auto mb-10 xl:max-w-xl xl:mx-0 xl:mb-16'>
+          </motion.h1>
+          {/* Subtitle */}
+          <motion.p 
+            variants={fadeIn('down', 0.3)} 
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='max-w-sm mx-auto mb-10 xl:max-w-xl xl:mx-0 xl:mb-16'
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eligendi at asperiores corrupti, dolore aut quos magnam, quas similique natus ipsum aliquam neque obcaecati placeat nisi veniam quae sed quibusdam.
-          </p>
+          </motion.p>
+          {/* My Projects button */}
           <div className='relative flex justify-center xl:hidden'>
             <ProjectsBtn/>
           </div>
+          <motion.div 
+            variants={fadeIn('down', 0.4)} 
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden xl:flex'
+          >
+            <ProjectsBtn/>
+          </motion.div>
         </div>
       </div>
+      {/*Background image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg img */}
+        <div className="absolute w-full h-full bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat mix-blend-color-dodge translate-z-0"></div>
+        <ParticlesContainer/>
+        {/* Avatar */}
+        <motion.div 
+          variants={fadeIn('up', 0.4)} 
+          transition={{ duration: 1, ease:'easeInOut' }}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className="w-full h-full max-w-[637px] max-h-[578px] absolute bottom-10 lg:right-[8%]"
+        >
+          <Avatar/>
+        </motion.div>
+      </div>
+      
     </div>
   )
 }
