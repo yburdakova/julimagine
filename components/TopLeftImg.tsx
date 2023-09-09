38884d015178db06a5e43c41df1135669a27328d
Yana Burdakova <bu.asti@gmail.com>
2023-09-08 21:35:27 -0400

    TopLeftImg: z-index  is changed

--- components/TopLeftImg.tsx
+++ components/TopLeftImg.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function TopLeftImg() {
   return (
-    <div className='absolute top-0 left-0 z-10 mix-blend-color-dodge w-[200px] xl:w-[400px] opacity-50'>
+    <div className='absolute top-0 left-0 z-4 mix-blend-color-dodge w-[200px] xl:w-[400px] opacity-50'>
       <Image src='/top-left-img.png' alt='Left image' width={400} height={400}/>
     </div>
   )
