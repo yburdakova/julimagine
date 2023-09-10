79676932a1c77fc5d73b8d5cac50877cd70b357c
Yana Burdakova <bu.asti@gmail.com>
2023-09-10 12:55:53 -0400

    Bulb component is completed

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../../variants';
+import { serviceData } from '../../constants';
+import { ServiceSlider, Bulb, Circles } from '../../components';
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className='flex items-center h-full bg-primary/30 py-36'>
+      <Circles/>
+      <div className="container mx-auto"></div>
+      <Bulb/>
+    </div>
   )
 }
 
