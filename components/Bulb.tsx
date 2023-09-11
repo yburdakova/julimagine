3d3e36f342695db665a4fd82c239b537846f9e67
Yana Burdakova <bu.asti@gmail.com>
2023-09-11 10:30:21 -0400

    Servise page and Slider component is done

--- components/Bulb.tsx
+++ components/Bulb.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 function Bulb() {
   return (
-    <div className='absolute duration-75 -left-32 -bottom-10 rotate-12 animate-pulse w-[200px] xl:w-[260px]'>
+    <div className='absolute duration-75 -left-32 -bottom-10 rotate-12 animate-pulse w-[200px] xl:w-[260px] '>
       <Image
         src={'/bulb.png'}
         alt='Bulb image'
         width={260}
         height={200}
-        className='w-full h-full'
+        className='z-0 w-full h-full'
       />
     </div>
   )
--- components/ServiceSlider.tsx
+++ components/ServiceSlider.tsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import { FreeMode, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+import {RxCrop, RxDesktop, RxPencil2, RxRocket, RxArrowTopRight} from 'react-icons/rx'
+
+import { serviceData } from '../constants';
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
-};
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView:1,
+          spaceBetween:15,
+        },
+
+        640: {
+          slidesPerView:3,
+          spaceBetween:15,
+        },
+        
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className='h-[240ppx] sm:h-[340px]'
+    >
+      {serviceData.map((item, index) => {
+        return <SwiperSlide key={index}>
+          <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+            {/* icon */}
+            <div className="mb-4 text-4xl text-accent">
+              {React.createElement(item.icon)}
+            </div>
+            {/* text */}
+            <div className="mb-8">
+              <div className="mb-2 text-lg">{item.title}</div>
+              <p className="max-w-[350px] leading-normal">{item.description}</p>
+            </div>
+            {/* arrow */}
+            <div className="text-3xl">
+              <RxArrowTopRight
+                className='transition-all duration-300 group-hover:rotate-45 group-hover:text-accent'
+              />
+            </div>
+          </div>
+        </SwiperSlide>
+      })}
+    </Swiper>
+
+)};
 
 export default ServiceSlider;
+
